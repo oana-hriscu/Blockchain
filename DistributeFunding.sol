a48965ca676058d359d7b56ad53218e1c357ef8c
oana-hriscu <oana.hriscu97@gmail.com>
2020-11-20 17:39:28 +0200

    Update DistributeFunding.sol

--- DistributeFunding.sol
+++ DistributeFunding.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: MIT
-
-
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.4.22 <=0.7.5;
 import 'OwnerProperty.sol';
 
 contract DistributeFunding is OwnerProperty{
     address crowdfundingOwner;
     mapping ( address => uint) shares;
-    address payable[] beneficiaries;    // lista de contribuitori ce vor primi o suma
+    address payable[] beneficiaries;
     uint funds = 0;
     uint currentSharePercentage = 0;
     
     event fundsReceived(uint _funds);
     
     
-    constructor() public {
-        crowdfundingOwner = msg.sender;   // adresa celui care trimite mesajul
+    constructor() {
+        crowdfundingOwner = msg.sender;
     }
    
-    function receiveFunds() payable external {
+    fallback() payable external{}
+    
+    receive() payable external{
         funds = msg.value;
         emit fundsReceived(funds);
     }
@@ -29,12 +29,12 @@ contract DistributeFunding is OwnerProperty{
         require(currentSharePercentage + _percentage <= 100, "The share increases the total share above 100%. Please pick a smaller value.");
         
         beneficiaries.push(_beneficiary);
-        shares[_beneficiary] = _percentage;  // unui beneficiar ii trimit x % 
+        shares[_beneficiary] = _percentage;
         currentSharePercentage += _percentage;
     }
     
     function distributeFunds() ownerOnly public{
-        require (funds > 0, "Not enough funds to distribute");
+        require (funds > 0, "Not enought funds to distribute");
         
         for (uint i = 0; i < beneficiaries.length; i++) {
             beneficiaries[i].transfer(shares[beneficiaries[i]] * funds/100);
